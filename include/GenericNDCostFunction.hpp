7fc50fa5df36387810ee1bbd915c8ce840a7a4bd
Martin Buist <biebml@nus.edu.sg>
2018-07-26 14:46:54 +0800

    Adding generic N-dimensional models

---
+++ include/GenericNDCostFunction.hpp
@@ -0,0 +1,130 @@
+#ifndef GENERICNDCOSTFUNCTION_HPP_
+#define GENERICNDCOSTFUNCTION_HPP_
+
+#include <vector>
+#include "GenericNDModel.hpp"
+#include "GenericCostFunction.hpp"
+
+/**
+ * This class acts as a cost function for any ND model, i.e., a model that
+ * derives from the GenericNDModel class. For example, you can pass your model
+ * and data to the constructor and a cost function will be created. You can
+ * then, for example, pass the result straight to any of the Unfit optimisers to
+ * find your model parameters. Of course you could also write your own cost
+ * function, or embed your model in a cost function, but why not let this class
+ * do the work for you. In terms of maths, if we write something like
+ * y = f(x0, x1, x2, ... ,xN), your model will implement f(x0, x1, x2, ... ,xN).
+ * Here we assume you have some data, y, x0, x1, ..., and you want to see if
+ * your model is a good fit to your y data. To do this we introduce a residual.
+ * For every data point, i, we will calculate the residual r_i = y_i - f(x_i).
+ * The cost of the model is the vector of these residuals.
+ */
+class GenericNDCostFunction : public Unfit::GenericCostFunction
+{
+ public:
+  /**
+   * We require the model to be passed in when we create one of these cost
+   * functions, so we must disallow use of the default constructor.
+   */
+  GenericNDCostFunction() = delete;
+
+  /**
+   * Create a cost function for a GenericNDModel. This requires that we pass in
+   * the model itself (model). If you use this constructor, make sure you pass
+   * the data to the cost function using the SetData function. Without data, it
+   * is not possible to calculate the cost of the model.
+   *
+   * \param model Your model, made by deriving from GenericNDModel
+   */
+  GenericNDCostFunction(GenericNDModel &model);
+
+  /**
+   * Create a cost function for a Generic2DModel. Here we pass in the model
+   * itself (model), and the data that we want to use to evaluate the model. For
+   * any model, the data takes the form of one or more independent variables
+   * (x0, x1, ... , xN) and a dependent variable (y). These could represent any
+   * type of data you like, e.g. strain and stress, or time and distance. Note
+   * that the data is not checked for consistency during construction, but it is
+   * checked before the data is used. You need to make sure that your data
+   * vectors are the same length (i.e., y, x0, x1, ... are the same length).
+   * If they are not, you will get an error when you try to use the data with
+   * your model.
+   *
+   * \param model Your model, made by deriving from GenericNDModel
+   * \param x A vector of vectors containing the independent variable values
+   *          for your data
+   * \param y A vector containing the dependent variable values for your data
+   */
+  GenericNDCostFunction(GenericNDModel &model,
+      const std::vector<std::vector<double>> &x, const std::vector<double> &y);
+
+  /**
+   * In case anyone wants to derive from this class in the future the destructor
+   * the should be virtual. The default destructor is fine.
+   */
+  virtual ~GenericNDCostFunction() = default;
+
+  /**
+   * For any model that follows the interface prescribed by GenericNDModel, this
+   * method takes in a vector of model parameters (c), evaluates the model using
+   * these parameters, and calculates the vector of residuals between the model
+   * output and the stored data. For each data point, i, the residual is
+   * calculated as r_i = y_i - f(x_i), where f(x_i) is the model evaluated at
+   * the point x_i with the provided parameters.
+   *
+   * \param c A vector of model parameters/constants
+   * \return A vector containing the residuals (costs) between the model and the
+   *           data
+   */
+  std::vector<double> operator()(const std::vector<double> &c);
+
+  /**
+   * This method takes in a set of data (independent = x, dependent = y) and
+   * checks that the data is valid before we try to use it. It checks that none
+   * of the vectors are empty and that they are all the same length (i.e., y,
+   * x0, x1, ... are the same length). If everything is okay then it will
+   * return true. If any issues are found it will return false.
+   *
+   * \param x A vector of vectors containing the independent variable values
+   * \param y A vector containing the dependent variable values
+   * \return true if the data is valid, otherwise false
+   */
+  bool CheckData(const std::vector<std::vector<double>> &x,
+      const std::vector<double> &y);
+
+  /**
+   * A simple method to extract a copy of the data that is currently stored
+   * in the cost function.
+   *
+   * \param x A vector of vectors containing the independent variable values
+   * \param y A vector containing the dependent variable values
+   */
+  void GetData(std::vector<std::vector<double>> &x, std::vector<double> &y);
+
+  /**
+   * This method takes in a set of data (independent = x, dependent = y) that
+   * then overwrites any data that is stored within the cost function. However,
+   * before anything is overwritten, CheckData is called to make sure the new
+   * data is valid. If the data vectors are empty, or are different lengths then
+   * the method will return false and nothing will be overwritten.
+   *
+   * \param x A vector of vectors containing the independent variable values
+   * \param y A vector containing the dependent variable values
+   * \return true if the data was successfully stored, otherwise false (in which
+   *           case the method did nothing)
+   */
+  bool SetData(const std::vector<std::vector<double>> &x,
+      const std::vector<double> &y);
+
+ private:
+  /** Store a reference to the model. */
+  GenericNDModel &model_;
+  /** Store the independent variable data so we don't pass it in each time. */
+  std::vector<std::vector<double>> x_;
+  /** Store the dependent variable data so we don't pass it in each time. */
+  std::vector<double> y_;
+  /** Store if valid data has been successfully added to the cost function */
+  bool has_data_;
+};
+
+#endif  // GENERICNDCOSTFUNCTION_HPP_
---
+++ include/GenericNDModel.hpp
@@ -0,0 +1,55 @@
+#ifndef GENERICNDMODEL_HPP_
+#define GENERICNDMODEL_HPP_
+
+#include <vector>
+
+/**
+ * This class provides an interface to what we call an n-dimensional, or ND
+ * model. What is a ND model? The simplest way to think of it is to consider a
+ * 2D model which is pretty much anything you could draw on a 2D graph
+ * with axes x and y, with a function y = f(x). Of course in your particular
+ * application you could have e.g. voltage as a function of time: v = f(t),
+ * or stress vs strain, or something completely different, but as long as you
+ * have one independent variable (e.g. x) and one independent variable (e.g. y),
+ * you have a 2D model. What if I have something more complicated, like stress
+ * and strain as a function of time, or stress = f(strain, time)? Well, then you
+ * have a 3D model, and we could write it in a general form like y = f(x0, x1).
+ * Here we have two independent variables that are needed to calculate our y.
+ * You can imagine that we could extend this to 4, or 5 or even N-dimensions.
+ * That is where the ND model comes in. In general we can write
+ * y = f(x0, x1, x2, ... , xN). Whether you have something like this, or
+ * something as simple as y = f(x), your model implementation should derive from
+ * this class so we keep a consistent interface to our modelling tools. Note
+ * that you can have as many parameters/constants as you like. You should look
+ * at our examples for further guidance if needed.
+ */
+class GenericNDModel
+{
+ public:
+  /**
+   * As we are deriving from this class, the destructor should be virtual. The
+   * default destructor is fine.
+   */
+  virtual ~GenericNDModel() = default;
+
+  /**
+   * All ND models must be implemented using this interface. For details of what
+   * constitutes an ND model please see the GenericNDModel class documentation.
+   * Here we want to pass in a vector of parameters (c), and a vector of vectors
+   * containing the independent variable values at which we want to evaluate our
+   * model (x). x[0] will contain a vector with the values of the first
+   * independent variable, x[1] will contain the second, if present, etc. The
+   * method should then return a vector containing the results of evaluating
+   * the model at each x value, i.e., y = f(x0, x1, ... , xN).
+   *
+   * \param c A vector of model parameters/constants
+   * \param x A vector of vectors containing the independent variable values
+   *          at which the model should be evaluated.
+   * \return  A vector containing the output from the model when evaluated at
+   *          x with parameters c.
+   */
+  virtual std::vector<double> operator()(const std::vector<double> &c,
+      const std::vector<std::vector<double>> &x) = 0;
+};
+
+#endif  // GENERICNDMODEL_HPP_
